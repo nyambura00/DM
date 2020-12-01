91fe6d100ac528fdb26a535d47b44ad9b586bad2
nyambura00 <winnienyambura14@gmail.com>
2020-12-01 11:31:44 +0300

    another program code style

--- magic.js
+++ magic.js
@@ -3,3 +3,15 @@ const phoneNumber = (arr) => {
 }
 
 console.log(phoneNumber([7, 0, 0, 4, 8, 2, 3, 9, 5]));
+
+
+//another version of my DM program
+
+const myNumber = (numbers) =>{
+    let template = "(xxx) xxx-xxxx"; //(700) 482-395
+    for(let i=0; i<template.length; i++){
+        template = template.replace('x', numbers[i] )
+    }
+    return template;
+}
+myNumber([7,0,0,4,8,2,3,9,5])
